14e6213c871d069f7b39aa7d5793740ae8d0e2e4
Ernestas Zalanskas <ezalanskas@gmail.com>
2022-01-27 17:55:09 +0200

    Revert "Delete vendor directory"
    This reverts commit 7ea3676e6b22749244d57d343f4b2123a8077698.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '0.0.2',
+        'version' => '0.0.2.0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'kcs/web',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'kcs/web' => array(
+            'pretty_version' => '0.0.2',
+            'version' => '0.0.2.0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
